6f472221f3c0f58483bd41ab7bb0d8056027b867
JLiu005 <JLiu005@gmail.com>
2017-02-28 11:19:32 +1300

    changed layout of checklist gui

--- process_manager/checklist_gui.py
+++ process_manager/checklist_gui.py
@@ -18,7 +18,7 @@ class Checkbar(Frame):
       for pick in picks:
          var = IntVar()
          chk = Checkbutton(self, text=pick, variable=var)
-         chk.pack(side=side, anchor=anchor, expand=YES)
+         chk.pack(side=TOP, anchor=anchor, expand=YES)
          self.vars.append(var)
    def state(self):
       return map((lambda var: var.get()), self.vars)
